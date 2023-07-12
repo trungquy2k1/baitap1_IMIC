a5948516ec242fa0721f01a44d7eb2b9f6b88c73
trungquy2k1 <trungquy1905@gmail.com>
2023-07-12 17:14:08 +0700

    bai3

--- App.tsx
+++ App.tsx
@@ -1,12 +1,9 @@
 import * as React from "react";
 import LoginScreen from "./Screen/LoginScreen";
+//import Login from "./Screen/Login";
 function App() {
-  return (
-    
-          <LoginScreen />
-       
-        
+  return ( 
+  <LoginScreen />   
   );
 }
 export default App;
-
--- Screen/LoginScreen.tsx
+++ Screen/LoginScreen.tsx
@@ -1,112 +1,163 @@
-import * as React  from "react";
-import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import React, {useState, useEffect} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Alert,
+} from 'react-native';
 
-const LoginScreen = () => {
+export default function LoginScreen() {
+  const [username, setUsername] = useState('');
+  const [pass, setPass] = useState('');
+  const [usernameError, setUsernameError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    Alert.alert(
+      'Thông báo',
+      'Ứng dụng muốn truy cập vào vị trí của bạn',
+      [
+        {
+          text: "Don't Allow",
+          onPress: () => console.log("Don't Allow Pressed"),
+          style: 'cancel',
+        },
+        {text: 'OK', onPress: () => console.log('OK Pressed')},
+      ],
+      {cancelable: false},
+    );
+  }, [6]);
+
+  const handleLogin = () => {
+    if (username === '') {
+        setUsernameError('Vui lòng điền thông tin');
+      } else {
+        setUsernameError('');
+      }
+  
+      if (pass === '') {
+        setPasswordError('Vui lòng điền thông tin');
+      } else {
+        setPasswordError('');
+      }
+  
+      if (username !== '' && pass !== '') {
+        setError('');
+        Alert.alert(`Chaof mừng ${username} đăng nhập`)
+      } else {
+        setError('Vui lòng điền đủ thông tin agggggggg');
+      }
+  };
+
   return (
-  <SafeAreaView style = {styles.root}>
-    <View style={styles.container }>
-      <Text style ={styles.text} > Yolo System </Text>
-      <TextInput style={styles.input} placeholder="Tên đăng nhập "/>
-      <TextInput style={styles.input} placeholder="Mật khẩu "/>
+    <SafeAreaView style={styles.root}>
+      <View style={styles.container}>
+        <Text style={styles.text}> Yolo System </Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Tên đăng nhập "
+          value={username}
+          onChangeText={text => setUsername(text)}
+        />
+        <Text style={{color: 'red'}}>{usernameError}</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Mật khẩu"
+          value={pass}
+          onChangeText={text => setPass(text)}
+        />
+        <Text style={{color: 'red'}}>{passwordError}</Text>
+        {/* <Text style={{color: 'red', alignSelf: 'center'}}>{error}</Text> */}
 
-      <TextInput style={styles.input} placeholder="Mật khẩu "/>
-      <TouchableOpacity style={styles.button}>
-        <Text style={{fontWeight:'bold',color:'white', fontSize:18}}>
-          LOGIN
-        </Text>
-      </TouchableOpacity>
-      <Text style={styles.tex1}>
-        OR
-      </Text>
-      <TouchableOpacity style={styles.button1}>
-        <Text style={{fontWeight:'bold',color:'white',fontSize:18}}>
-          Facebook
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button2}>
-        <Text style={{fontWeight:'bold',color:'white',fontSize:18}}>
-          Google
-        </Text>
-      </TouchableOpacity>
-    </View>
-  </SafeAreaView>
- );
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={{fontWeight: 'bold', color: 'white', fontSize: 18}}>
+            LOGIN
+          </Text>
+        </TouchableOpacity>
+        <Text style={styles.tex1}>OR</Text>
+        <TouchableOpacity style={styles.button1}>
+          <Text style={{fontWeight: 'bold', color: 'white', fontSize: 18}}>
+            Facebook
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button2}>
+          <Text style={{fontWeight: 'bold', color: 'white', fontSize: 18}}>
+            Google
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
 
-};
-
-const styles = StyleSheet.create ({
-  root :{
+const styles = StyleSheet.create({
+  root: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    marginVertical:140,
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: 'gray',
+    marginVertical: 140,
+   
   },
-  container:{
-
+  container: {
     // borderRadius: 10,
     // borderWidth: 1,
     // borderColor: 'gray',
   },
-  text :{
-    color : 'green',
+  text: {
+    color: 'green',
     fontSize: 50,
-    fontWeight:'700',
-    margin : 8,
-    textAlign:'center',
-    justifyContent:'center',
-    textAlignVertical:'center',
-
+    fontWeight: '700',
+    margin: 8,
+    textAlign: 'center',
+    justifyContent: 'center',
+    textAlignVertical: 'center',
   },
-  
-  input:{
-      height:40,
-      width:370,
-      marginHorizontal: 16,
-      borderColor:'gray',
-      borderRadius:10,
-      borderWidth:1,
-      marginTop:10,
 
+  input: {
+    height: 40,
+    width: 370,
+    marginHorizontal: 16,
+    borderColor: 'gray',
+    borderRadius: 10,
+    borderWidth: 1,
+    marginTop: 10,
   },
-  button:{
-    height:40,
-    margin:4,
-    backgroundColor:'#99109c',
-    alignItems:'center',
-    justifyContent:'center',
-    borderRadius:60,
+  button: {
+    height: 40,
+    margin: 4,
+    backgroundColor: '#99109c',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 60,
     marginHorizontal: 16,
   },
-  tex1 :{
-    color:'green',
-    fontSize:40,
-    fontWeight:'700',
-    textAlign:'center',
-    marginVertical:20
-
+  tex1: {
+    color: 'green',
+    fontSize: 40,
+    fontWeight: '700',
+    textAlign: 'center',
+    marginVertical: 20,
   },
-  button1:{
-    height:40,
-    margin:4,
-    backgroundColor:'#0b8a0b',
-    alignItems:'center',
-    justifyContent:'center',
-    borderRadius:60,
+  button1: {
+    height: 40,
+    margin: 4,
+    backgroundColor: '#0b8a0b',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 60,
     marginHorizontal: 16,
-    
   },
-  button2:{
-    height:40,
-    margin:4,
-    backgroundColor:'#e01d1d',
-    alignItems:'center',
-    justifyContent:'center',
-    borderRadius:60,
+  button2: {
+    height: 40,
+    margin: 4,
+    backgroundColor: '#e01d1d',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 60,
     marginHorizontal: 16,
   },
 });
-
-export default LoginScreen;
